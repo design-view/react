d313a8d57ad4dc3b2cf4b5795156d4d5989ce689
design-view <pink_candy02@naver.com>
2021-06-21 03:43:29 +0900

    배열 렌더링 하기

--- src/UserList.js
+++ src/UserList.js
@@ -18,22 +18,25 @@ function UserList() {
             email: "min@example.com"
         }
     ];
-  
+    function User({ user }){
+        return (
+            <div>
+                <b>{user.username}</b>
+                <span>({user.email})</span>
+            </div>
+        )
+    }
     return (
         <div>
-            <div>
-                <b>{users[0].username}</b>
-                <span>({users[0].email})</span>
-            </div>
-            <div>
-                <b>{users[1].username}</b>
-                <span>({users[1].email})</span>
-            </div>
-            <div>
-                <b>{users[2].username}</b>
-                <span>({users[2].email})</span>
-            </div>
+        {/* <User user={users[0]} />
+        <User user={users[1]} />
+        <User user={users[2]} /> */}
+        
+        {
+            users.map(user => (<User user={user} key={user.id} />)
+            )
+        }
         </div>
-    )
+    );
 }
 export default UserList;
